d8881404dd9493a001e06bf923192301ac72d718
Axel-Coelho <66997267+Axel-Coelho@users.noreply.github.com>
2022-02-17 11:17:45 +0100

    Fix bugs tests

--- src/main/kotlin/com/teaching/project/banking/domain/Account.kt
+++ src/main/kotlin/com/teaching/project/banking/domain/Account.kt
@@ -3,14 +3,31 @@ package com.teaching.project.banking.domain
 data class Account(val iban: String, var balance: Double = 0.0) {
 
     init {
-        // initialization
+        if (!Regex(pattern = "^FR[0-9]{25}").matches(iban)) {
+            throw RuntimeException ("IBAN Invalide")
+        }
     }
 
-    fun deposit(amount: Double) {}
+    fun deposit(amount: Double) {
+        balance+=amount
+    }
 
-    fun withdraw(amount: Double) {}
+    fun withdraw(amount: Double) {
+        if(balance - amount > 0){
+            balance-=amount;
+        }else{
+            throw RuntimeException("Vous n'avez pas assez d'argent.")
+        }
+    }
 
     fun transfer(receiver: Account, amount: Double){
+        if(amount > 0){
+            withdraw(amount)
+            receiver.deposit(amount)
+        }else{
+            throw RuntimeException("Votre demande de transfert est invalide.")
+        }
+
     }
 
 }
--- src/main/kotlin/com/teaching/project/banking/domain/Bank.kt
+++ src/main/kotlin/com/teaching/project/banking/domain/Bank.kt
@@ -3,9 +3,16 @@ package com.teaching.project.banking.domain;
 data class Bank(val accounts: MutableMap<String, Account> = mutableMapOf()) {
 
     fun register(account: Account) {
+        accounts.put(account.iban, account)
     }
 
-    fun transfer(sender: Account, receiver: Account, amount: Double) {}
+    fun transfer(sender: Account, receiver: Account, amount: Double) {
+        if (amount < 0) {
+            throw RuntimeException ("Transfert invalide.")
+        } else {
+            sender.transfer(receiver, amount)
+        }
+    }
 
     fun dashboard() {
         print(this)
